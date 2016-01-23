28367925247e8a728adf24959ad6db5ab6c4c0ed
Hu Songtao <idisblueflash@gmail.com>
2016-01-23 22:44:44 +0800

    split config with webpack-merge
    and enable HotModuleReplacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
 	app: path.join(__dirname, 'app'),
 	build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
 	// Entry accepts a path or an object of entries.
 	// The build chapter contains an example of the latter.
 	entry: PATHS.app,
@@ -14,3 +17,26 @@ module.exports = {
 		filename: 'bundle.js'
 	}
 };
+
+// Default configuration
+if(TARGET === 'start' || !TARGET) {
+	module.exports = merge(common, {
+		devServer: {
+			contentBase: PATHS.build,
+			historyApiFallback: true,
+			hot: true,
+			inline: true,
+			progress: true,
+			stats: 'errors-only',
+			host: process.env.HOST,
+			port: process.env.PORT
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin()
+		]
+	});
+}
+
+if(TARGET === 'build') {
+	module.exports = merge(common, {});
+}
