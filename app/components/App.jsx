e9e7bfbc1fcb2e46755a77d61147c598463ccca9
Hu Songtao <idisblueflash@gmail.com>
2016-01-23 23:47:36 +0800

    make drill can be modify

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,21 +2,27 @@ import uuid from 'node-uuid';
 import React from 'react';
 
 export default class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			drills: [
+				{
+					id: uuid.v4(),
+					label: 'a)',
+					answer: '',
+					key:'FF'
+				},
+				{
+					id: uuid.v4(),
+					label: 'b)',
+					answer: '',
+					key:'FF'
+				}
+			]
+		}
+	}
 	render() {
-		const drills = [
-			{
-				id: uuid.v4(),
-				label: 'a)',
-				answer: '',
-				key:'FF'
-			},
-			{
-				id: uuid.v4(),
-				label: 'b)',
-				answer: '',
-				key:'FF'
-			}
-		];
+		const drills = this.state.drills;
 		return (
 			<div>
 				<ul>{drills.map(drill =>
