936e9e2c7864a8a4c0f0de8983d89535bc2d38b6
Hu Songtao <idisblueflash@gmail.com>
2016-01-23 23:44:51 +0800

    default list

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,28 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
 
 export default class App extends React.Component {
 	render() {
-		return <Note />;
+		const drills = [
+			{
+				id: uuid.v4(),
+				label: 'a)',
+				answer: '',
+				key:'FF'
+			},
+			{
+				id: uuid.v4(),
+				label: 'b)',
+				answer: '',
+				key:'FF'
+			}
+		];
+		return (
+			<div>
+				<ul>{drills.map(drill =>
+					<li key={drill.id}>{drill.label}</li>
+					)}</ul>
+				</div>
+		);
 	}
 }
