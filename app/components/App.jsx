19aebff987dc69a8546395df13284e3a0ff5482f
Hu Songtao <idisblueflash@gmail.com>
2016-01-28 09:53:38 +0800

    update record

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -74,6 +74,7 @@ export default class App extends React.Component {
 				{date: '06/1/26 09:07', spend: 969,  errors: 3, notes: '2. e) h)'},
 				{date: '06/1/26 10:40', spend: 1029, errors: 1, notes: '1. b)'},
 				{date: '06/1/27 09:26', spend: 1399, errors: 6, notes: '1. j) 2. i) 3. c) j) k) f)'},
+				{date: '06/1/27 13:45', spend: 1372, errors: 6, notes: '2. b) c)'},
 			]
 		};
 	}
